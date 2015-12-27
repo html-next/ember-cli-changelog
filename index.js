6814baba86ed4c3f209959ab9298cf7e0c83f1ca
Miguel Andrade <jmandrade0@gmail.com>
2015-12-27 17:03:36 +0000

    make script a command

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 /* jshint node: true */
 'use strict';
 
+var commands = require('./lib/commands');
+
 module.exports = {
-  name: 'ember-cli-changelog'
+  name: 'ember-cli-changelog',
+
+  includedCommands: function() {
+    return commands;
+  }
 };
